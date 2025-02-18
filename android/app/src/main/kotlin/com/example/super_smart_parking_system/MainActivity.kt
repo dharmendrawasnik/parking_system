88ef1925ba16508f9dcfb8a34bc93e911abd0b1e
dharmendra <dharmendrawasnik823@gmail.com>
2025-02-18 05:59:06 +0530

    this first commit

---
+++ android/app/src/main/kotlin/com/example/super_smart_parking_system/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.super_smart_parking_system
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/Screens/Splash_Screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:super_smart_parking_system/Screens/Login_Screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+
+    super.initState();
+  }
+
+  void avoidFunction(){
+    Future.delayed(Duration(seconds: 3),(){
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>LoginScreen()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Scaffold(
+      body: Center(
+        child: Text('Splash_Screen'),
+      ),
+    ));
+  }
+}
